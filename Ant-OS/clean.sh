037687ce30443be2ce383cbffad62836f4fcc08b
gowthamreddysomala <gowthamreddysomala@gmail.com>
2024-07-09 12:51:02 +0530

    V_1.0.0

---
+++ Ant-OS/clean.sh
@@ -0,0 +1,2 @@
+sudo rm -r output/*.*  output/loader  output/iso output/build_date
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+sudo mkarchiso  -v -w /home/gowthamreddy/Extracted/Ant-OS/Ant-OS/output  -o /home/gowthamreddy/Extracted/Ant-OS/Ant-OS/output  Ant-OS/
+
