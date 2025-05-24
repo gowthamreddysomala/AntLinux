ec1107738f01881a810338a64afcb16ff68dc9b1
Gowtham Reddy <gowthamreddysomala@gmail.com>
2025-05-24 19:21:12 +0530

    V1.2

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-sudo mkarchiso  -v -w /home/gowthamreddy/Extracted/Ant-OS/Ant-OS/output  -o /home/gowthamreddy/Extracted/Ant-OS/Ant-OS/output  Ant-OS/
+sudo mkarchiso  -v -w /home/gowthamreddy/Extracted/AntLinux/Ant-OS/output  -o /home/gowthamreddy/Extracted/Ant-OS/Ant-OS/output  Ant-OS/
 
+# Modify the path but the syntax is same change folder name if needed to Ant-OS if you encounter errors
